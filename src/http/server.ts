f311941ce9a7ad8e1026395693a5dd9a27b12070
DanielEgiidio <danielegidiot@gmail.com>
2024-09-11 15:53:36 -0300

    feat: finalização do backend

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,39 +4,31 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { createGoal } from '../functions/create-goal'
-import z from 'zod'
+
+import fastifyCors from '@fastify/cors'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-// Add schema validator and serializer
+app.register(fastifyCors, {
+  origin: '*',
+})
+
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async request => {
-    const { title, desiredWeeklyFrequency } = request.body
-
-    await createGoal({
-      title,
-      desiredWeeklyFrequency,
-    })
-  }
-)
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server running')
+    console.log('HTTP server running!')
   })
